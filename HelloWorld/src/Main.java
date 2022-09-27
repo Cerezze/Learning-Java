2770353b23a94ce42098cc06cdea4110835cfa25
unknown <gm.c.burzo@gmail.com>
2022-09-26 21:59:12 -0400

    init1

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Cerezze!");
+        int myFirstNumber = (9 + 5) + (2 * 10);
+
+        System.out.println(myFirstNumber);
+    }
+}
