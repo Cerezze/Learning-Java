8340b51b82ed43b2f128ac344432c7d2a8f8bc16
unknown <gm.c.burzo@gmail.com>
2022-09-27 20:36:55 -0400

    i

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Cerezze!");
-        int myFirstNumber = (9 + 5) + (2 * 10);
-
-        System.out.println(myFirstNumber);
+        System.out.println("Hello world!");
     }
 }
