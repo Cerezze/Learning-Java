d02ce282a8b851336792610844a0cf38a1446eea
unknown <gm.c.burzo@gmail.com>
2022-09-28 21:09:39 -0400

    i

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,5 +1,165 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello, Tim");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
+
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
+        System.out.println("Busted MIN value = " + (myMinIntValue - 1));
+
+        int myMaxIntTest = 2_147_483_647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+        long myLongValue = 100;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
+        long bigLongLiteralValue = 2_147_483_647_234L;
+        System.out.println(bigLongLiteralValue);
+
+        short bigShortLiteralValue = 32767;
+
+        System.out.println("Before Conversion" + myMinByteValue);
+
+        int myNewByteValue = (int) (myMinByteValue / 2);
+
+        System.out.println("After Conversion" + myMinByteValue);
+
+        short myNewShortValue = (short) (myMinShortValue / 2);
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " +myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double maximum value = " +myMaxDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue = 5.25f;
+        double myDoubleValue = 5.25d;
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+
+        // byte
+        // short
+        // int
+        // long
+        // float
+        // double
+        // char
+        // boolean
+        String myString = "This is a string";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + ", and this is more.";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + " \u00A9 2019";
+        System.out.println("myString is equal to " + myString);
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println(numberString);
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("LastString is equal to " + lastString);
+        double doubleNumber = 120.47d;
+        lastString = lastString + doubleNumber;
+        System.out.println("LastString is equal to " + lastString);
+
+        int result = 1 + 2; // 1 + 2 = 3
+        System.out.println("1 + 2 = " + result);
+        int previousResult = result;
+        System.out.println("previousResult =  " + previousResult);
+        result = result - 1;    // 3 - 1 = 2
+        System.out.println("3 - 1 = " + result);
+        System.out.println("previousResult =  " + previousResult);
+
+        result = result * 10;   // 2 * 10 = 20
+        System.out.println("2 * 10 = " + result);
+
+        result = result / 5;    // 20 / 5 = 4
+        System.out.println("20 / 5 = " + result);
+
+        result = result % 3;    // the remainder of (4 % 3) = 1
+        System.out.println("4 % 3 = " + result);
+
+        result = 1 + 2; // 1 + 2 = 3
+        System.out.println("1 + 2 = " + result);
+        previousResult = result;
+        System.out.println("previousResult =  " + previousResult);
+        result = result - 1;    // 3 - 1 = 2
+        System.out.println("3 - 1 = " + result);
+        System.out.println("previousResult =  " + previousResult);
+
+        result = result * 10;   // 2 * 10 = 20
+        System.out.println("2 * 10 = " + result);
+
+        result = result / 5;    // 20 / 5 = 4
+        System.out.println("20 / 5 = " + result);
+
+        result = result % 3;    // the remainder of (4 % 3) = 1
+        System.out.println("4 % 3 = " + result);
+
+        // result = result + 1;
+        result++; // 1 + 1 = 2
+        System.out.println("1 + 1 = " + result);
+
+        result--;  // 2 - 1 = 1
+        System.out.println("2 - 1 = " + result);
+
+        // result = result +2;
+        result += 2;  // 1 + 2 = 3
+        System.out.println("1 + 2 = " + result);
+
+        // result = result * 10;
+        result *= 10; // 3 * 10 = 30
+        System.out.println("3 * 10 = " + result);
+
+        // result = result / 3
+        result /= 3;  // 30 / 3 = 10
+        System.out.println("30 / 3 = " + result);
+
+        // result = result - 2
+        result -= 2;  // 10 - 2 = 8
+        System.out.println("10 - 2 = " + result);
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
     }
 }
